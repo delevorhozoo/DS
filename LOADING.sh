463c90fd805f04265437640592222baa5e1b9f43
delevorhozoo <hozoohozoo61@gmail.com>
2025-10-29 02:52:34 +0700

    Create LOADING.sh

---
+++ LOADING.sh
@@ -0,0 +1,10 @@
+# 1. Update paket Termux
+pkg update && pkg upgrade
+
+# 2. Instal Python, Git, dan Chromium (browser)
+pkg install python git chromium
+
+# 3. Instal library Selenium dan webdriver-manager
+pip install selenium webdriver-manager
+
+python 
